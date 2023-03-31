14034108b17ae89840122a334044d0c0d38229c9
Ryan Andersen <rustydustyryan@gmail.com>
2023-03-31 12:15:37 -0600

    added new songs

--- database/week_3.js
+++ database/week_3.js
@@ -1,52 +1,52 @@
 addWeekSongs(3, "3/31/2023", [
   {
-    artist: "Mr. Brown",
-    songname: "An Extra Day",
-    pathname: "An Extra Day.mp3",
+    artist: "The Antlers",
+    songname: "Rolled Together",
+    pathname: "Rolled Together.mp3",
   },
   {
-    artist: "Jakob Ogawa",
-    songname: "You And I",
-    pathname: "You And I.mp3"
+    artist: "As Tall As Lions",
+    songname: "Into the Flood",
+    pathname: "Into the Flood.mp3"
   },
   {
-    artist: "Desmond Cheese",
-    songname: "Memories of a Dream",
-    pathname: "Memories of a Dream.mp3"
+    artist: "Galimatias",
+    songname: "South",
+    pathname: "South.mp3"
   },
   {
-    artist: "Leon Bridges",
-    songname: "Bad Bad News",
-    pathname: "Bad Bad News.mp3"
+    artist: "Andrew Applepie",
+    songname: "Don't Fuck This Up (feat. Ed Prosek)",
+    pathname: "Don't Fuck This Up (feat. Ed Prosek).mp3"
   },
   {
-    artist: "Franc Moody",
-    songname: "Dopamine",
-    pathname: "Dopamine.mp3"
+    artist: "Sjowgren",
+    songname: "Waiting Room",
+    pathname: "Waiting Room.mp3"
   },
   {
-    artist: "Dirtwire",
-    songname: "Amphibian Circuits",
-    pathname: "Amphibian Circuits.mp3"
+    artist: "Aphex Twin",
+    songname: "Avril 14th",
+    pathname: "Avril 14th.mp3"
   },
   {
-    artist: "Feiertag",
-    songname: "Run Away (feat. Pip Millett)",
-    pathname: "Run Away (feat. Pip Millett).mp3"
+    artist: "Damu The Fudgemunk",
+    songname: "Sensory Rebirth (2004)",
+    pathname: "Sensory Rebirth (2004).mp3"
   },
   {
-    artist: "Son Little",
-    songname: "ASAP",
-    pathname: "ASAP.mp3"
+    artist: "Gotye",
+    songname: "Learnalilgivinanlovin",
+    pathname: "Learnalilgivinanlovin.mp3"
   },
   {
-    artist: "Oddisee",
-    songname: "Born Before Yesterday",
-    pathname: "Born Before Yesterday.mp3"
+    artist: "Atoms For Peace",
+    songname: "Before Your Very Eyes",
+    pathname: "Before Your Very Eyes.mp3"
   },
   {
-    artist: "Crumb",
-    songname: "Bones",
-    pathname: "Bones.mp3"
+    artist: "City and Colour",
+    songname: "Fragile Bird",
+    pathname: "Fragile Bird.mp3"
   }
 ]);

50493c128992abf8cbbdd108ca3c33ca6785a15d
Ryan Andersen <rustydustyryan@gmail.com>
2023-04-07 16:06:31 -0600

    added new music

---
+++ database/week_4.js
@@ -0,0 +1,52 @@
+addWeekSongs(4, "3/31/2023", [
+  {
+    artist: "Beauville",
+    songname: "Letting In (Prod. NoMBe)",
+    pathname: "Letting In [Prod. NoMBe].mp3",
+  },
+  {
+    artist: "Saturna",
+    songname: "Call to the Unknown",
+    pathname: "Call to the Unknown.mp3"
+  },
+  {
+    artist: "Santport",
+    songname: "Neverending Naps",
+    pathname: "neverending naps.mp3"
+  },
+  {
+    artist: "Leisure",
+    songname: "All Over You",
+    pathname: "All Over You.mp3"
+  },
+  {
+    artist: "J Lloyd",
+    songname: "Apocalypse/If I Fall Under",
+    pathname: "Apocalypse_If I Fall Under.mp3"
+  },
+  {
+    artist: "Lindsey Lomis",
+    songname: "Simple Love",
+    pathname: "Simple Love.mp3"
+  },
+  {
+    artist: "Jaguar Sun",
+    songname: "Forever and More",
+    pathname: "Forever and More.mp3"
+  },
+  {
+    artist: "Brooksley Born",
+    songname: "Starts And Stops",
+    pathname: "04 Starts And Stops.mp3"
+  },
+  {
+    artist: "IDK",
+    songname: "Zaza Tree (prod. KAYTRANADA)",
+    pathname: "Zaza Tree (prod. KAYTRANADA).mp3"
+  },
+  {
+    artist: "Young The Giant",
+    songname: "Empire Ants (Gorillaz Cover)",
+    pathname: "Empire Ants (Gorillaz Cover).mp3"
+  }
+]);

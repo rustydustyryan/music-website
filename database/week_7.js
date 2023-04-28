85a5a0a1444d69c7e0ab3f047433a5f1d6cfcc0f
Ryan Andersen <rustydustyryan@gmail.com>
2023-04-28 13:46:22 -0600

    added week 7 songs

---
+++ database/week_7.js
@@ -0,0 +1,52 @@
+addWeekSongs(7, "4/28/2023", [
+  {
+    artist: "Broken Social Scene",
+    songname: "Sweetest Kill",
+    pathname: "Sweetest Kill.mp3",
+  },
+  {
+    artist: "Lo Moon",
+    songname: "Loveless (Edit)",
+    pathname: "Loveless (Edit).mp3"
+  },
+  {
+    artist: "Elio",
+    songname: "@Elio.irl",
+    pathname: "@Elio.irl.mp3"
+  },
+  {
+    artist: "OLIVIA",
+    songname: "Reason To Stay",
+    pathname: "Reason To Stay.mp3"
+  },
+  {
+    artist: "Half Moon Run",
+    songname: "Then Again",
+    pathname: "Then Again.mp3"
+  },
+  {
+    artist: "Shakey Graves",
+    songname: "House of Winston",
+    pathname: "House of Winston.mp3"
+  },
+  {
+    artist: "Karate Boogaloo",
+    songname: "Any Time, Any Place [Janet Jackson cover]",
+    pathname: "Any Time, Any Place [Janet Jackson cover].mp3"
+  },
+  {
+    artist: "LUMP",
+    songname: "Late To The Flight",
+    pathname: "Late To The Flight.mp3"
+  },
+  {
+    artist: "Eefje de Visser",
+    songname: "Ongeveer",
+    pathname: "Ongeveer.mp3"
+  },
+  {
+    artist: "Nick Hakim",
+    songname: "Bet She Looks Like You",
+    pathname: "Bet She Looks Like You.mp3"
+  }
+]);

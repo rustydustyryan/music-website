835deafbe1e349153972dcb31a2aeb3724827623
Ryan Andersen <rustydustyryan@gmail.com>
2023-05-11 16:38:16 -0600

    added week 8 songs

---
+++ database/week_8.js
@@ -0,0 +1,52 @@
+addWeekSongs(8, "5/5/2023", [
+  {
+    artist: "Kings Of Convenience",
+    songname: "Misread",
+    pathname: "Misread.mp3",
+  },
+  {
+    artist: "Grizzly Bear",
+    songname: "Speak In Rounds",
+    pathname: "Speak In Rounds.mp3"
+  },
+  {
+    artist: "Josh Rouse",
+    songname: "Come Back",
+    pathname: "Come Back.mp3"
+  },
+  {
+    artist: "Geronimus Brothers",
+    songname: "Just Another Weekday pt. 1 (feat. Obie Iyoha)",
+    pathname: "Just Another Weekday pt. 1 (feat. Obie Iyoha).mp3"
+  },
+  {
+    artist: "Shallou",
+    songname: "After",
+    pathname: "After.mp3"
+  },
+  {
+    artist: "Kye Kye",
+    songname: "People",
+    pathname: "People.mp3"
+  },
+  {
+    artist: "Alfred Hall",
+    songname: "Someplace Beautiful",
+    pathname: "Someplace Beautiful.mp3"
+  },
+  {
+    artist: "Little Dragon",
+    songname: "Crystalfilm",
+    pathname: "Crystalfilm.mp3"
+  },
+  {
+    artist: "James Vincent McMorrow",
+    songname: "Get Low",
+    pathname: "Get Low.mp3"
+  },
+  {
+    artist: "Flamingosis",
+    songname: "Want Me (Need Me)",
+    pathname: "Want Me (Need Me).mp3"
+  }
+]);

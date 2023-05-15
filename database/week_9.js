e8164072a893a3b8799028885136a857b0ad1c3d
Ryan Andersen <rustydustyryan@gmail.com>
2023-05-15 17:43:26 -0600

    added week 9 songs

---
+++ database/week_9.js
@@ -0,0 +1,52 @@
+addWeekSongs(9, "5/12/2023", [
+  {
+    artist: "Trevor Hall",
+    songname: "The Lime Tree",
+    pathname: "The Lime Tree.mp3",
+  },
+  {
+    artist: "Bahamas",
+    songname: "Lost In The Light",
+    pathname: "Lost In The Light.mp3"
+  },
+  {
+    artist: "Mansionair",
+    songname: "Easier",
+    pathname: "Easier.mp3"
+  },
+  {
+    artist: "Lorn",
+    songname: "BATTY'S THEME",
+    pathname: "BATTY'S THEME.mp3"
+  },
+  {
+    artist: "Marc Rebillet",
+    songname: "YOU AND ME",
+    pathname: "YOU AND ME.mp3"
+  },
+  {
+    artist: "Cafune",
+    songname: "Tek It",
+    pathname: "Tek It.mp3"
+  },
+  {
+    artist: "Myles Morgan",
+    songname: "IKYWC",
+    pathname: "IKYWC.mp3"
+  },
+  {
+    artist: "Robohands",
+    songname: "Green",
+    pathname: "Green.mp3"
+  },
+  {
+    artist: "M Field",
+    songname: "Fiona",
+    pathname: "Fiona.mp3"
+  },
+  {
+    artist: "Adam Melchor",
+    songname: "Real Estate",
+    pathname: "Real Estate.mp3"
+  }
+]);

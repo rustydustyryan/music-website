187a76779af12cd60d0a5fde9535f232c29bd02d
Ryan Andersen <rustydustyryan@gmail.com>
2023-05-26 13:21:50 -0600

    added week 11 songs

---
+++ database/week_11.js
@@ -0,0 +1,52 @@
+addWeekSongs(11, "5/26/2023", [
+  {
+    artist: "Rabino",
+    songname: "Crocodile",
+    pathname: "Crocodile.mp3",
+  },
+  {
+    artist: "Monsters Of Folk",
+    songname: "Dear God",
+    pathname: "Dear God.mp3"
+  },
+  {
+    artist: "Circula",
+    songname: "For Esbjorn",
+    pathname: "For Esbjorn.mp3"
+  },
+  {
+    artist: "Bayonne",
+    songname: "Spectrolite",
+    pathname: "Spectrolite.mp3"
+  },
+  {
+    artist: "Diiv",
+    songname: "Taker",
+    pathname: "Taker.mp3"
+  },
+  {
+    artist: "Last Lynx",
+    songname: "None of My Friends",
+    pathname: "None of My Friends.mp3"
+  },
+  {
+    artist: "LUMP",
+    songname: "Curse of the Contemporary",
+    pathname: "Curse of the Contemporary.mp3"
+  },
+  {
+    artist: "Alice Coltrane",
+    songname: "Turiya And Ramakrishna",
+    pathname: "Turiya And Ramakrishna.mp3"
+  },
+  {
+    artist: "Phony Ppl",
+    songname: "End Of The niGht. (Louis Futon Remix)",
+    pathname: "End Of The niGht. (Louis Futon Remix).mp3"
+  },
+  {
+    artist: "Joker",
+    songname: "On My Mind",
+    pathname: "On My Mind.mp3"
+  }
+]);

0d11e53d1eea0c7b3d255686e933cea5544f5f3b
Ryan Andersen <rustydustyryan@gmail.com>
2023-07-04 12:09:16 -0600

    started over

--- src/common.ts
+++ src/common.ts
@@ -1,167 +1,78 @@
-const SONGS: Song[] = (window as any).SONGS = [];
-const WEEKS: Week[] = (window as any).WEEKS = [];
+createPlayer('JMSN', "'Bout It");
+createPlayer('Daniel Feels', "Behave");
+createPlayer('Elhae', "Halfway Love (Prod. by Rascal)");
+createPlayer('Max Pope', "Just Friends");
 
-type Week = {
-  order: number;
-  date: Date;
-  songs: Song[];
+
+function createPlayer(artist: string, title: string) {
+  const containerEl = document.querySelector('main');
+  containerEl.innerHTML += `
+  <div class="player">
+    <audio>
+      <source src="media/${title}.mp3" type="audio/mpeg" />
+    </audio>
+    <h1>${artist} - <small>${title}</small></h1>
+    <div class="progress-container">
+      <div class="progress"></div>
+    </div>
+    <div class="icons">
+      <div class="btn prev-btn"><i class="fa-solid fa-backward fa-2x"></i></div>
+      <div class="btn play-btn"><i class="fa-solid fa-play fa-2x"></i></div>
+      <div class="btn pause-btn hidden"><i class="fa-solid fa-pause fa-2x"></i></div>
+      <div class="btn next-btn"><i class="fa-solid fa-forward fa-2x"></i></div>
+    </div>
+  </div>
+  `
+
+  const audioEl = Array.from(document.querySelectorAll('audio')).pop();
+  const playerEl = Array.from(document.querySelectorAll('.player')).pop();
+  const playBtn = Array.from(document.querySelectorAll('.play-btn')).pop();
+  // const pauseBtn = Array.from(document.querySelectorAll('.pause-btn')).pop();
+  // const nextBtn = document.querySelector('.next-btn').addEventListener('click', nextSong);
+  // const prevBtn = document.querySelector('.prev-btn').addEventListener('click', prevSong);
+  // const progressContainer = document.querySelector('.progress-container');
+  // const progress = document.querySelector('.progress');
+
+  playerEl.addEventListener('mouseover', () => {
+    console.log('clicked');
+  })
+
+  console.log(playBtn);
+
+  function playAudio() {
+    console.log('playing')
+    audioEl.play();
+    // playBtn.classList.add('hidden');
+    // pauseBtn.classList.remove('hidden');
+    //   if (audioEl.paused) {
+    //     audioEl.play();
+    //   }
+    }
+  
+  // function pauseAudio() {
+  //   const audioEl = document.querySelector('audio');
+  //   audioEl.pause();
+  //   playBtn.classList.remove('hidden');
+  //   pauseBtn.classList.add('hidden');
+  //     if (audioEl.play) {
+  //       audioEl.pause()
+  //     }
+  //   }
+  
+  // function nextSong() {
+  //   const audioEl = document.querySelector('audio');
+  //   audioEl.pause();
+  //   audioEl.currentTime = 0;
+  //   audioEl.play();
+  // }
+  
+  // function prevSong() {
+  //   const audioEl = document.querySelector('audio');
+  //   audioEl.pause();
+  //   audioEl.currentTime = 0;
+  //   audioEl.play();
+  // }
 }
 
-type Song = {
-  artist: string;
-  songname: string;
-  pathname: string;
-};
-
-function addSongs(parentElement: HTMLElement, songs: Song[], includeAudio: boolean) {
-  for (let songIndex = 0; songIndex < songs.length; songIndex++) {
-    const song = songs[songIndex];
-    const songContainer = addElement(parentElement, "div", `artist-${songIndex + 1}`);
-    addSong(song, songIndex, songContainer, includeAudio);
-  }
-}
-
-function addSong(song: Song, songIndex: number, songContainer: HTMLElement, includeAudio: boolean) {
-  const h1 = addElement(songContainer, "h1");
-
-  const artist = addElement(h1, "span", "artist-color");
-  artist.textContent = song.artist;
-
-  const space = addElement(h1, "span");
-  space.textContent = " - ";
-
-  const songname = addElement(h1, "small");
-  songname.textContent = song.songname;
-
-  if (!includeAudio) return;
-
-  const progressContainer = addElement(songContainer, "div", "progress-container");
-  const progressInner = addElement(progressContainer, "div", "progress");
-
-  const audio = document.createElement("audio");
-  audio.className = "audio";
-  audio.src = "media/" + song.pathname;
-  songContainer.appendChild(audio);
-
-  const musicNav = addElement(songContainer, "div", "music-nav");
-
-  const prev = addButton("prev", "backward");
-  const play = addButton("play action-btn-big", "play");
-  const next = addButton("next", "forward");
-
-  /**
-   * Binds to prev button. Plays the previous song in the list.
-   */
-  prev.addEventListener("click", () => {
-    for (let si = 0; si < SONGS.length; si++) {
-      pauseSong(si);
-    }
-    let prevIndex = songIndex - 1;
-    if (prevIndex === -1) {
-      prevIndex = 0;
-    }
-    playSong(prevIndex);
-  });
-
-  /**
-   * Binds to next button. Plays the next song in the list.
-   */
-  next.addEventListener("click", () => {
-    for (let si = 0; si < SONGS.length; si++) {
-      pauseSong(si);
-    }
-    let nextIndex = songIndex + 1;
-    if (nextIndex === SONGS.length) {
-      nextIndex = 0;
-    }
-    playSong(nextIndex);
-  });
-
-  /**
-   * Updates the progress bar to show the current progress of the song.
-   */
-  function updateProgress(e) {
-    const { duration, currentTime } = e.srcElement;
-    const progressPercent = (currentTime / duration) * 100;
-    progressInner.style.width = `${progressPercent}%`;
-  }
-
-  function setProgress(e: MouseEvent) {
-    const width = this.clientWidth;
-    const clickX = e.offsetX;
-    const duration = audio.duration;
-
-    audio.currentTime = (clickX / width) * duration;
-  }
-
-  /**
-   * Binds to play button. Plays or pauses the current song.
-   */
-  play.addEventListener("click", () => {
-    if (audio.paused) {
-      playSong(songIndex);
-    } else {
-      pauseSong(songIndex);
-    }
-
-    for (let i = 0; i < SONGS.length; i++) {
-      if (i !== songIndex) {
-        const otherAudio = document.getElementsByClassName("audio")[i] as HTMLAudioElement;
-        if (!otherAudio.paused) {
-          pauseSong(i);
-        }
-      }
-    }
-  });
-
-  /**
-   * Progress listener. Updates the progress bar.
-   */
-  audio.addEventListener("timeupdate", updateProgress);
-
-  progressContainer.addEventListener("click", setProgress);
-
-  /**
-   * Song ended listener. Plays the next song in the list.
-   */
-  audio.addEventListener("ended", () => {
-    stopSong(songIndex);
-    if (songIndex === 9) {
-      playSong(0);
-    } else {
-      playSong(songIndex + 1);
-    }
-  });
-
-  /**
-   * Inserts a button with an icon
-   */
-  function addButton(className: string, icon: string) {
-    const button = addElement(musicNav, "button", "action-btn " + className);
-    addElement(button, "i", "fas fa-" + icon);
-    return button;
-  }
-}
-
-function addWeekSongs(week: number, date: string, songs: Song[]) {
-  WEEKS.push({
-    order: week,
-    date: new Date(date),
-    songs: songs
-  });
-  for (const song of songs) {
-    SONGS.push(song);
-  }
-}
-
-/**
- * Inserts a new element into the DOM
- */
-function addElement(parent: HTMLElement, tag: string, className?: string) {
-  const child = document.createElement(tag);
-  if (className) {
-    child.className = className;
-  }
-  parent.appendChild(child);
-  return child;
-}
+// let songIndex = 1;
+//   const songs = ['media/Bout It.mp3', 'media/Behave.mp3', 'media/Halfway Love (Prod. by Rascal).mp3', 'media/Just Friends.mp3'];
